b3f3ebd89bbc5f877b87b3639fe4852017ad2406
Charpell <charpellumeh@gmail.com>
2018-07-09 23:22:57 +0100

    Complete Search functunality with the searchbox input field when the state change

--- src/App.js
+++ src/App.js
@@ -2,14 +2,35 @@ import React, { Component } from 'react';
 
 import { robots } from './robots';
 import CardList from './components/CardList';
+import SearchBox from './components/SearchBox';
 
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    }
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+  }
+
   render() {
+    const { searchfield } = this.state;
+
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
+
     return (
-      <div className="App">
-        <CardList robots={robots} />
+      <div className='tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <CardList robots={filteredRobots} />
       </div>
     );
   }
