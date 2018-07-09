9e1d4ccc451ac21aa8057888b68c2ed87edbf167
Charpell <charpellumeh@gmail.com>
2018-07-09 02:39:37 +0100

    Change background with Particle.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 
 import Logo from './components/Logo/Logo';
 import Navigation from './components/Navigation/Navigation';
@@ -7,10 +8,28 @@ import Rank from './components/Rank/Rank';
 
 import './App.css';
 
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className='particles'
+          params={particlesOptions}
+        />
         <Navigation />
         <Logo />
         <Rank />
