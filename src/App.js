35db9ded2d767e60b51f90b8820a799759308826
Charpell <charpellumeh@gmail.com>
2018-07-09 02:06:52 +0100

    Build Navigations and Install tachyons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
+
+import Navigation from './components/Navigation/Navigation';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Boiler Plate</h1>
+        <Navigation />
+        {/* <Logo />
+        <ImageLinkForm />
+        <FaceRecognition /> */}
       </div>
     );
   }
