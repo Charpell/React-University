ed1648b0782aea77a377de20376911e7a9bf53e3
Charpell <charpellumeh@gmail.com>
2018-07-09 23:45:22 +0100

    Get our data from external endpoint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-import { robots } from './robots';
 import CardList from './components/CardList';
 import SearchBox from './components/SearchBox';
 
@@ -10,9 +9,15 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> response.json())
+      .then(users => {this.setState({ robots: users})});
   }
 
   onSearchChange = (event) => {
@@ -20,17 +25,19 @@ class App extends Component {
   }
 
   render() {
-    const { searchfield } = this.state;
+    const { robots, searchfield } = this.state;
 
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
 
-    return (
+    return !robots.length ?
+    <h1>Loading</h1> :
+    (
       <div className='tc'>
-        <h1>RoboFriends</h1>
+        <h1 className='f1'>RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={filteredRobots} />
+          <CardList robots={filteredRobots} />
       </div>
     );
   }
