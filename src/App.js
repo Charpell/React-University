4b2887cf8e6e89bb42e83e3fcd7409d68c03668d
Charpell <charpellumeh@gmail.com>
2018-11-18 07:30:00 +0100

    Composition: Breaking App into Component

--- src/App.js
+++ src/App.js
@@ -5,14 +5,45 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header>
-          <h1>Scoreboard</h1>
-          <span className="stats">Players: 1</span>
-        </header>
+      <div className="scoreboard">
+        <Header />
+
+        <Player />
       </div>
     );
   }
+}
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Scoreboard</h1>
+      <span className="stats">Players: 1</span>
+    </header>
+  );
+}
+
+
+const Player = () => {
+  return (
+    <div className="player">
+      <span className="player-name">
+        Ebuka
+      </span>
+
+      <Counter />      
+    </div>
+  )
+}
+
+const Counter = () => {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <span className="counter-score">35</span>
+      <button className="counter-action increment"> + </button>
+    </div>
+  )
 }
 
 export default App;
