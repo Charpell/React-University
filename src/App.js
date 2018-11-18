2cebe85e3c1926fc13943b5f3588f1f6667adecd
Charpell <charpellumeh@gmail.com>
2018-11-18 07:18:23 +0100

    Project Setup for Scoreboard

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Boiler Plate</h1>
+        <header>
+          <h1>Scoreboard</h1>
+          <span className="stats">Players: 1</span>
+        </header>
       </div>
     );
   }
