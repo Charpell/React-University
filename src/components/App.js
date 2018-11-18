936ab085dc714ccd63dc5335c1c34f63f5dbd845
Charpell <charpellumeh@gmail.com>
2018-11-18 11:30:26 +0100

    Components

---
+++ src/components/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+import '../App.css';
+import Header from './Header';
+import Player from './Player';
+
+
+
+class App extends Component {
+  state = {
+    players : [{
+      name: "Guil",
+      id: 1
+    },
+    {
+      name: "Treasure",
+      id: 2
+    },
+    {
+      name: "Ashley",
+      id: 3
+    },
+    {
+      name: "James",
+      id: 4
+    }]
+  }
+
+  handleRemovePlayer = id => {
+    this.setState(prevState => {
+      return {
+        players: prevState.players.filter(p => p.id !== id)
+      }
+    })
+  }
+
+  render() {
+    const { players } = this.state;
+    
+    return (
+      <div className="scoreboard">
+        <Header title="Scoreboard" totalPlayers={players.length} />
+
+        {players.map(player => (
+          <Player 
+            name={player.name}
+            id={player.id}
+            key={player.id.toString()}
+            removePlayer={this.handleRemovePlayer}
+          />
+        ))}
+      
+      </div>
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+export default App;
