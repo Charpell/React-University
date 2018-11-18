3d10438696c9fea1064dda1ba14c5fa4ecdfbad7
Charpell <charpellumeh@gmail.com>
2018-11-19 00:00:14 +0100

    Remove a guest

--- src/App.js
+++ src/App.js
@@ -43,6 +43,14 @@ class App extends Component {
 
   toggleEditingAt = index => this.toggleGuestPropertyAt("isEditing", index);
 
+  removeGuestAt = index => 
+    this.setState({
+      guests: [
+        ...this.state.guests.slice(0, index),
+        ...this.state.guests.slice(index + 1)
+      ]
+    })
+
   setNameAt = (name, indexToChange) =>
     this.setState({
       guests: this.state.guests.map((guest, index) => {
@@ -129,7 +137,8 @@ class App extends Component {
             toggleConfirmationAt={this.toggleConfirmationAt}
             toggleEditingAt={this.toggleEditingAt}
             setNameAt={this.setNameAt}
-            isFiltered={this.state.isFiltered} 
+            isFiltered={this.state.isFiltered}
+            removeGuestAt={this.removeGuestAt} 
           />
         </div>
       </div>
