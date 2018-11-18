3b9ff6391c9bd0317a300cd2f3fc579cf4e5e31f
Charpell <charpellumeh@gmail.com>
2018-11-18 21:01:36 +0100

    Use Pure Component to prevent wasteful rerender

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 
 import Counter from './Counter';
 
-const Player = (props) => {
-  return (
-    <div className="player">
-      <span className="player-name">
-        <button 
-          className="remove-player" 
-          onClick={ () => props.removePlayer(props.id) }>
-            x
-        </button>
-        { props.name }
-      </span>
-
-      <Counter 
-        score={props.score} 
-        changeScore={props.changeScore}
-        index={props.index}
-      />      
-    </div>
-  )
+class Player extends PureComponent {
+  render() {
+    return (
+      <div className="player">
+        <span className="player-name">
+          <button 
+            className="remove-player" 
+            onClick={ () => this.props.removePlayer(this.props.id) }>
+              x
+          </button>
+          { this.props.name }
+        </span>
+  
+        <Counter 
+          score={this.props.score} 
+          changeScore={this.props.changeScore}
+          index={this.props.index}
+        />      
+      </div>
+    )
+  }
 }
 
 export default Player;
