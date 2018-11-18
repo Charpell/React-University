c647084087a1caef76a762d677e3c1495a043d9f
Charpell <charpellumeh@gmail.com>
2018-11-18 23:41:43 +0100

    Filtering only Confirmed Guest

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import GuestList from './GuestList';
 
 class App extends Component {
   state = {
+    isFiltered: false,
     guests: [
       {
         name: 'Treasure',
@@ -55,7 +56,8 @@ class App extends Component {
     })
 
 
-
+  toggleFilter = () => 
+    this.setState({ isFiltered: !this.state.isFiltered })
 
   getTotalInvited = () => this.state.guests.length;
 
@@ -75,7 +77,10 @@ class App extends Component {
           <div>
             <h2>Invitees</h2>
             <label>
-              <input type="checkbox" /> Hide those who haven't responded
+              <input 
+                type="checkbox"
+                onChange={this.toggleFilter} 
+                checked={this.state.isFiltered} /> Hide those who haven't responded
             </label>
           </div>
           <table className="counter">
@@ -99,7 +104,8 @@ class App extends Component {
             guests={this.state.guests}
             toggleConfirmationAt={this.toggleConfirmationAt}
             toggleEditingAt={this.toggleEditingAt}
-            setNameAt={this.setNameAt} 
+            setNameAt={this.setNameAt}
+            isFiltered={this.state.isFiltered} 
           />
         </div>
       </div>
