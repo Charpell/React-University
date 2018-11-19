dd80b391efebab46cd8ea19b3885b419967d0ecf
Charpell <charpellumeh@gmail.com>
2018-11-19 08:41:22 +0100

    Use URL Parameters to declare dynamic routes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {    
   
@@ -11,6 +12,8 @@ class Home extends Component {
         <p>We have thousands of videos created by expert teachers on web design and front end development. Our library is continually refreshed with the latest on web technology so you will never fall behind.</p>
         <hr />
         
+        <h3>Featured Teachers</h3>
+        <Link to="teachers/HTML/Tommy-Wingo">Tommy Wingo </Link>
       </div>
     );
   }
