99eefa7681580c8465bb2fdf290d8891168141fe
Charpell <charpellumeh@gmail.com>
2018-11-20 17:39:01 +0100

    Returning null from setState

--- src/App.js
+++ src/App.js
@@ -5,17 +5,42 @@ import './App.css';
 import StudentForm from './components/StudentForm';
 import ErrorBoundary from './components/ErrorBoundary';
 import JSTeachers from './JSTeachers';
-
+import Teacher from './Teacher';
 class App extends Component {
 
+  state = {
+    teacher: ''
+  };
+
+  updateTeacher = teacher => {
+    const newTeacher = teacher;
+    this.setState(state => {
+      if (state.teacher === newTeacher) {
+        return null;
+      } else {
+        return { teacher };
+      }
+    });
+  }
   
   render() {
+    const teachers = ['jay', 'vivianne', 'ecma', 'json'];
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <header>
+          <h1>Meet the Teachers</h1>
+          {teachers.map((teacher,i) =>
+            <button
+              key={teacher}
+              type="button"
+              value={teacher}
+              onClick={e => this.updateTeacher(e.target.value)}
+            >{teacher}
+            </button>)}
         </header>
+
+        <Teacher teacher={this.state.teacher} />
 
         <ErrorBoundary>
           <StudentForm />
