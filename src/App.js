ad788b7521f7be95de6c4c258b7009fb6d5186a6
Charpell <charpellumeh@gmail.com>
2019-01-26 14:14:16 +0100

    Deleting a movie

--- src/App.js
+++ src/App.js
@@ -10,24 +10,39 @@ class App extends Component {
     movies: getMovies()
   }
 
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies 
+    })
+  }
+
   render() {
     return (
-      <table class="table">
+      <table className="table">
         <thead>
           <tr>
             <th scope="col">Title</th>
             <th scope="col">Genre</th>
             <th scope="col">Stock</th>
             <th scope="col">Rate</th>
+            <th scope="col"></th>
           </tr>
         </thead>
         <tbody>
           { this.state.movies.map(movie => (
-            <tr>
+            <tr key={movie._id}>
               <th scope="row">{movie.title}</th>
               <td>{movie.genre.name}</td>
               <td>{movie.numberInStock}</td>
               <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button 
+                  className="btn btn-danger btn-sm"
+                  onClick={() => this.handleDelete(movie)}
+                  >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
           
