74902a8c6681dde19e15855d98b7d738ccc96858
Charpell <charpellumeh@gmail.com>
2019-01-26 13:29:51 +0100

    Building the Movies component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import { getMovies } from './services/fakeMovieService'
+
+import Movies from './components/movies';
 
 import './App.css';
 
 class App extends Component {
+  state = {
+    movies: getMovies()
+  }
+
   render() {
     return (
-      <main class="container">
-        <h1>Hello World</h1>
-      </main>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Title</th>
+            <th scope="col">Genre</th>
+            <th scope="col">Stock</th>
+            <th scope="col">Rate</th>
+          </tr>
+        </thead>
+        <tbody>
+          { this.state.movies.map(movie => (
+            <tr>
+              <th scope="row">{movie.title}</th>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+            </tr>
+          ))}
+          
+        </tbody>
+      </table>
     );
   }
 }
