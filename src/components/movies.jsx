4462f107fb202bb4c433a3c5e002baf45ac6d24f
Charpell <charpellumeh@gmail.com>
2019-01-27 09:05:05 +0100

    Sorting- Moving responsibility

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -45,16 +45,7 @@ class Movies extends Component {
     this.setState({ selectedGenre: genre, currentPage: 1 })
   }
 
-  handleSort = path => {
-    const sortColumn = {...this.state.sortColumn}
-
-    if (sortColumn.path === path)
-      sortColumn.order = (sortColumn.order === 'asc') ? 'desc' : 'asc'
-    else {
-      sortColumn.path = path;
-      sortColumn.order = 'asc'
-    }
-
+  handleSort = sortColumn => {
     this.setState({ sortColumn  })
   }
 
@@ -86,6 +77,7 @@ class Movies extends Component {
           <p>Showing {filtered.length} movies in the database.</p>
           <MoviesTable 
             movies={movies} 
+            sortColumn={sortColumn}
             onLike={this.handleLike} 
             onDelete={this.handleDelete}
             onSort={this.handleSort}
