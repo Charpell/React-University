07d2af5927ee9666982fe7284666db30ca3c5d11
Charpell <charpellumeh@gmail.com>
2019-02-01 08:23:36 +0100

    Confirmation Button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
+import ConfirmButton from './ConfirmButton';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Boiler Plate</h1>
+        <ConfirmButton 
+          times={3}
+          dialog={['Delete', 'Are you Sure?', 'Once more to delete']}
+          action={() => console.log('action!')} 
+        />
       </div>
     );
   }
