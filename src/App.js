6ba824d0c7a372f10f56fbc730cdb1fbe08e2c0a
Charpell <charpellumeh@gmail.com>
2019-02-01 06:43:13 +0100

    Create a Toggle Componnt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 
+import Toggle from './Toggle';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Boiler Plate</h1>
+        <Toggle>
+          <h1>Hello</h1>
+        </Toggle>
       </div>
     );
   }
