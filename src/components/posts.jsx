afd38b71f55d2d16ff1c072d8ace823fd286a2f5
Charpell <charpellumeh@gmail.com>
2019-02-02 14:11:00 +0100

    Optional String Parameters

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import queryString from 'query-string';
 
-const Posts = ({ match }) => {
+// http://localhost:3000/posts/2019/05?sortBy=newest&approved=true
+
+const Posts = ({ match, location }) => {
+  const result = queryString.parse(location.search)
+  console.log(result)
+  
   return (
     <div>
       <h1>Posts</h1>
