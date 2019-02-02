eac9456dfba3ecbf9df8e43ce7663f80ab9f490c
Charpell <charpellumeh@gmail.com>
2019-02-02 08:35:29 +0100

    Extracting a Table

---
+++ src/common/table.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import TableHeader from './common/tableHeader';
+import TableBody from './common/tableBody';
+
+const Table = (props) => {
+  const { columns, sortColumn, onSort, data } = props;
+
+  return ( 
+    <table className="table">
+        <TableHeader 
+          columns={columns} 
+          sortColumn={sortColumn} 
+          onSort={onSort} 
+        />
+
+        <TableBody 
+          columns={columns}
+          data={data} 
+        />
+        
+      </table>
+   );
+}
+ 
+export default Table;
