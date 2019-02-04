74d7a1721ca588084e43babd7d423cc0110d930d
Charpell <charpellumeh@gmail.com>
2019-02-04 06:05:25 +0100

    Controlled Elements

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 
 class LoginForm extends Component {
-  username = React.createRef();
+  state = {
+    account: { 
+      username: '',
+      password: ''
+    }
+  }
 
   handleSubmit = e => {
     e.preventDefault();
 
-    const username = this.username.current.value;
-    console.log(username)
+    console.log(this.state.account.username)
+  }
+
+  handleChange = e => {
+    const account = {...this.state.account};
+    account.username = e.currentTarget.value;
+    this.setState({ account })
   }
 
   render() { 
@@ -17,12 +27,12 @@ class LoginForm extends Component {
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input 
-              ref={this.username} 
+            <input  
               id="username" 
               type="text" 
               className="form-control" 
-              autoFocus
+              value={this.state.account.username}
+              onChange={this.handleChange}
             />
           </div>
           <div className="form-group">
