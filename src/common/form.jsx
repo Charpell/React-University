f13204dd26ab04814f3229c89ac701a07ce62108
Charpell <charpellumeh@gmail.com>
2019-02-04 11:49:19 +0100

    Create Movie Form

--- src/common/form.jsx
+++ src/common/form.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Joi from 'joi-browser';
 import Input from './input';
+import Select from './select';
 
 class Form extends Component {
   state = { 
@@ -9,25 +10,21 @@ class Form extends Component {
   };
 
   validate = () => {
-    const { error } = Joi.validate(this.state.account, this.schema, { abortEarly: false });
-
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.data, this.schema, options);
     if (!error) return null;
 
     const errors = {};
-
-    // Map an array to an Object. Alternativey you can use reduce
-    for (let item of error.details)
-      errors[item.path[0]] = item.message
-
-    return errors
-  }
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
+  };
 
   validateProperty = ({ name, value }) => {
     const obj = { [name]: value };
-    const schema = { [name]: this.schema[name] }
-    const {error} = Joi.validate(obj, schema);
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
     return error ? error.details[0].message : null;
-  }
+  };
 
   handleSubmit = e => {
     e.preventDefault();
@@ -47,9 +44,9 @@ class Form extends Component {
     if (errorMessage) errors[input.name] = errorMessage;
     else delete errors[input.name]
 
-    const account = {...this.state.account};
-    account[input.name] = input.value;
-    this.setState({ account, errors })
+    const data = {...this.state.data};
+    data[input.name] = input.value;
+    this.setState({ data, errors })
   }
 
   renderButton(label) {
@@ -73,6 +70,21 @@ class Form extends Component {
           error= {errors[name]}
         />
     )
+  }
+
+  renderSelect(name, label, options) {
+    const { data, errors } = this.state;
+
+    return (
+      <Select
+        name={name}
+        value={data[name]}
+        label={label}
+        options={options}
+        onChange={this.handleChange}
+        error={errors[name]}
+      />
+    );
   }
 
 }
